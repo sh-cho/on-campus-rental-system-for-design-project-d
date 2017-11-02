bf6ceff82eb84cc0bb72c7a832a4130ee4e05651
Seonghyeon Cho <seonghyeoncho96@khu.ac.kr>
2017-11-02 09:42:13 +0900

    add some basic structure

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var app = express();
+var router = require('./router/main')(app);
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
 
-app.listen(3000, function () {
+var server = app.listen(3000, function () {
   console.log('Listening on port 3000');
 });
+
+app.use(express.static('public'));
