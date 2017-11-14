b032bb9bba89a2e037917fa2ecd0c33b44734049
Seonghyeon Cho <seonghyeoncho96@khu.ac.kr>
2017-11-15 00:12:13 +0900

    add favicon

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var session = require('express-session');
+var favicon = require('serve-favicon');
+const path = require('path');
 var app = express();
 var router = require('./router/router')(app);
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
+app.use(favicon(path.join(__dirname, 'public/image', 'favicon.ico')));
 
 var server = app.listen(3000, function () {
   console.log('Listening on port 3000');
