ad3990cda9da5c5fcea323a6be2c709eab65d3da
Seonghyeon Cho <seonghyeoncho96@khu.ac.kr>
2017-12-05 05:23:50 +0900

    express-session test

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 //import libraries
 const express = require('express');
 const session = require('express-session');
+const bcrypt = require('bcrypt-nodejs');
 const favicon = require('serve-favicon');
 const path = require('path');
 const morgan = require('morgan');
+const ejs = require('ejs');
 
 const app = express();
 const router = require('./router/router.js')(app);
@@ -14,9 +16,34 @@ const mylogger = morgan('tiny');
 //server settings
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
+app.engine('html', ejs.renderFile);
+
+app.use(session({
+    secret: 'test',
+    resave: false,
+    saveUninitialized: true
+}));
 app.use(favicon(path.join(__dirname, 'public/image', 'favicon.ico')));
 app.use(express.static('public'));
 app.use(mylogger);
+
+
+const users = [
+    {
+        user_id: 'hyeok',
+        user_nickname: '혁',
+        user_pwd: '123456'
+    },
+    {
+        user_id: 'hyc7575',
+        user_nickname: '에이치',
+        user_pwd: '1q2w3e4r'
+    }
+];
+
+
+//for pretty print
+app.locals.pretty = true;
 
 //create server and listen
 const server = app.listen(3000, function () {
--- router/router.js
+++ router/router.js
@@ -1,9 +1,24 @@
 module.exports = function (app) {
     //Pug render
     app.get('/', (req, res) => {
-        res.render('index');
+        res.render('index.pug');
     });
     app.get('/pug-test', (req, res) => {
-        res.render('pug-test', {time: Date(), _title: 'PugPug'});
+        res.render('pug-test.pug', {time: Date(), _title: 'PugPug'});
+    });
+    app.get('/login', (req, res) => {
+        res.render('login.pug');
+    });
+    app.get('/session-test', (req, res) => {
+        const sess = req.session;
+        res.render('session-test.pug', {
+            // nickname: sess.user_uid+1 ? app.users[sess.user_uid]['user_nickname'] : ''
+            nickname: 'abc'
+        });
+    });
+
+    //html render
+    app.get('/html-test', (req, res) => {
+        res.render('html-test.html');
     });
 };
