ffa0a18f038aa55a7ba161fd3eeced778ef59329
오형민 <mihn0621@naver.com>
2017-12-16 16:55:45 +0900

    class 추가

--- class/lecture.js
+++ class/lecture.js
@@ -1,5 +1,8 @@
 class Lecture {
-    constructor() {
-
+    constructor(classroom_id, dayofthe_week, start_time, end_time) {
+    	this._classroom_id = classroom_id;
+    	this._dayofthe_week = dayofthe_week;
+    	this._start_time = start_time;
+    	this._end_time = end_time;
     }
 }
